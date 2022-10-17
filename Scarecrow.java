844a93dcbd81fc4c40570e9a7b8311a248138122
Vivian Wei <vwei@smith.edu>
2022-10-17 15:34:13 -0400

    assemble scarecrow

--- Scarecrow.java
+++ Scarecrow.java
@@ -11,34 +11,39 @@ class Scarecrow {
      * TODO: Uncomment remaining parts as you implement each class
      */
     private Pumpkin head;
-    // private Shirt body;
-    // private Pants legs;
+    private Shirt body;
+    private Pants legs;
     // private Hat hat;
-    // private Boot leftFoot;
-    // private Boot rightFoot;
+    private Boot BothFoot;
     // private Banner sign;
     private String message;
 
     /* Constructor
      * TODO: initialize remaining parts
      */
-    public Scarecrow(Pumpkin h) {
+    public Scarecrow(Pumpkin h, Shirt b, Pants l,Boot f) {
         head = h;
-    }
+        body = b;
+        legs = l;
+        BothFoot = f;
 
+    }
     /* Displays the Scarecrow 
      * TODO: call the .display() method of each part... 
      *       ...in the right order!
     */
     public void display() {
         head.display();
+        body.display();
+        legs.display();
+        BothFoot.display();
     }
 
     /* Main method (for testing) */
     public static void main(String[] args) {
 
         // TODO: Don't forget to update the line below if you modify the constructor
-        Scarecrow myScarecrow = new Scarecrow(new Pumpkin());
+        Scarecrow myScarecrow = new Scarecrow(new Pumpkin(),new Shirt(), new Pants(), new Boot("both"));
 
         // If a message was passed in on the command line, extract and store it
         // TODO: in Step 4, you'll pass this value along to your Banner constructor
